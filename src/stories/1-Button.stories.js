4d9db2398591ab70137fe2277d76875cf5d87e51
Hakan Demiral <hakan.98@outlook.com>
2021-06-13 16:53:09 +0300

    button story extended

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,41 +1,23 @@
 import React from 'react';
 import Button from '../Components/Generic/Button';
+import * as Icons from '../Components/icons';
 
 export default {
   title: 'Buttons',
   component: Button,
+  args: {
+    text: 'Your text is here',
+    type: 'primary',
+    size: 'long',
+  },
+  argTypes: {
+    icon: {
+      control: {
+        type: 'select',
+        options: Object.keys(Icons),
+      },
+    },
+  },
 };
 
-export const Secondary = () => (
-  <>
-    <Button text="Little Button" size="little" />
-    <br />
-    <Button text="Little Button" size="little" icon="ExternalLink" disabled />
-    <br />
-    <Button text="Normal Button" />
-    <br />
-    <Button text="Small Button" size="short" icon="Bell" />
-    <br />
-    <Button text="Disabled Button" disabled />
-    <br />
-    <Button text="Disabled short Button" size="short" disabled />
-  </>
-);
-
-export const Primary = () => (
-  <>
-    <Button type="primary" text="Little Button" size="little" />
-    <br />
-    <Button type="primary" text="Little Button" size="little" icon="ExternalLink" disabled />
-    <br />
-    <Button type="primary" text="Normal Button" />
-    <br />
-    <Button type="primary" text="Small Button" size="short" />
-    <br />
-    <Button type="primary" text="Disabled Button" disabled />
-    <br />
-    <Button type="primary" text="Disabled Small Button" size="short" disabled />
-    <br />
-    <Button type="primary" text="Button with icon" icon="Eye" />
-  </>
-);
+export const Secondary = Button.bind({});
