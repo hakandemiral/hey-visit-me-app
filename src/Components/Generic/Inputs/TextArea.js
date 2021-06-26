e7610d79bb8683355f2fb759151d2bfe07edcc59
Hakan Demiral <hakan.98@outlook.com>
2021-06-26 18:44:01 +0300

    dropdown component fix

---
+++ src/Components/Generic/Inputs/TextArea.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Box = styled.input(({
+  theme,
+}) => css``);
+
+const UploadBox = () => (
+  <Box>
+    asd
+  </Box>
+);
+
+export default UploadBox;
